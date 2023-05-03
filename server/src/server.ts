aaef58677290d15941b7cfb0c5c00fac74293017
alicebranq <branq.alice@gmail.com>
2023-05-03 20:36:21 -0300

    feat: :sparkles: add método post
    adição do método POST para criação de um novo hábito

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,13 @@
 import Fastify from "fastify"; 
 import cors from "@fastify/cors"
-import { PrismaClient } from "@prisma/client"
+import { prisma } from "./lib/prisma"
+import { appRoutes } from "./lib/routes";
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async () => {
-    const habits = await prisma.habit.findMany()
-
-    return 'somethingsomenthing'
-})
 
 app.listen({
     port: 3333,
